6cbde22606c785795b233e2b1b781096a1e8739b
lucaslemos94 <lc.lemos@outlook.com>
2021-04-07 13:06:54 -0300

    Adding contacts CRUD.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :contacts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   #controller + action (function)
@@ -6,5 +7,5 @@ Rails.application.routes.draw do
 
 
   get 'about', to: 'static_pages#about'
-  get 'contact', to: 'static_pages#contact'
+  get 'address', to: 'static_pages#address'
 end
